d9c78466233d07deb9faa5afe70008a71ab74cd7
OmMTelrandhe <139479968+OmMTelrandhe@users.noreply.github.com>
2024-06-04 11:47:12 +0530

    Update script.js

--- About/script.js
+++ About/script.js
@@ -1,6 +1,6 @@
 function redirectToIndex() {
     if(document.getElementById("email").value != "" && document.getElementById("name").value != ""){
         alert("Thank you for your feedback");
-    window.location.href = '/Health-Assistant/About/index.html';
+    window.location.href = 'index.html';
     }
 }
